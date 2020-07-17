2147907b60ddf7e0084368eb74f332cf6af00fb3
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-16 23:56:07 -0300

    Create ex002.py

---
+++ udemy/exercícios_seção_4/ex002.py
@@ -0,0 +1,9 @@
+2.	# Faça um programa que leia um número real (qualquer número existente) e o imprima na tela
+# print('O número digitado foi', n)
+# print('O número', n, 'foi digitado')
+
+n = str(input('Digite um número real :'))
+print(f'O número digitado foi {n}')
+print('O número {} foi digitado'.format(n))
+print('O número digitado foi', n)
+print('O número', n, 'foi digitado')
