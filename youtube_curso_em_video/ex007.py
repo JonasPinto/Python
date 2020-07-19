f1cdd36e60e3fe98e0b2a1ecf8406fa9128f6ee4
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-19 11:40:33 -0300

    create ex006 and ex007

---
+++ youtube_curso_em_video/ex007.py
@@ -0,0 +1,6 @@
+# desenvolva um programa que leia notas de um aluno e mostre sua media
+
+n1 = float(input('Digite a primeira nota '))
+n2 = float(input('Digite a segunda  nota '))
+
+print(f'A média entre as duas notas é de \033[31m{(n1 + n2) / 2 }')
