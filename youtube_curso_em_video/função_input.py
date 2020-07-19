1e9dadf22e305c0a8a53965aa2c6d65f38298713
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-19 11:05:58 -0300

    Create função_input.py

---
+++ youtube_curso_em_video/função_input.py
@@ -0,0 +1,5 @@
+# faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+
+nome = str(input('Digite o seu nome\n'))
+print(f'Olá \033[31m{nome}\033[m é um prazer conhece-lo')
+m          
