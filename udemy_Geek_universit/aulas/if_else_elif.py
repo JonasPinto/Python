65857f7b414f33a6de9b17b10c7506518e515cc7
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-21 19:45:24 -0300

    Create if_else_elif.py

---
+++ udemy_Geek_universit/aulas/if_else_elif.py
@@ -0,0 +1,34 @@
+"""
+Eatruturas condicionais
+if(se), else(senao), elif(senao se)
+
+# Estrutura condicional if, else em c
+if(idade < 18){
+    printf("Menor de idade");
+}else if(idade == 18){
+   printf("Tem 18 anos");
+}else{
+  printf("Maior de idade");
+}
+
+# Estrutura condicional if, else em java
+if(idade < 18){
+   system.out.println("Menor de idade");
+}else if(idade == 18){
+   system.out.println("Tem 18 anos");
+}else{
+   system.out.println("Maior de idade");
+}
+"""
+
+idade = 17
+if idade < 18:
+    print('Menor de idade')
+elif idade == 18:
+    print('Tem 18 anos')
+else:
+    print('Maior de idade')
+
+
+
+
