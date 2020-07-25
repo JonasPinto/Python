e5a04665e09098e81ceb7df6b3d2271a239adfd5
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-25 15:48:34 -0300

    new exercises

---
+++ udemy_Geek_universit/exercícios_seção_4/ex009.py
@@ -0,0 +1,5 @@
+# Leia uma temperatura em graus Celsius e apresente-a convertida em graus Kelvin. K = C + 273.15
+
+c = float(input('Digite a temperatura em C° celsius: '))
+k = c + 273.15
+print(f'{c}° são {k}° graus Kelvin')
