9e9a349ab3c6b100614d4a4021f61bbd5a641f26
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-26 00:49:32 -0300

    creat ex031

--- udemy_Geek_universit/exercícios_seção_5/ex030.py
+++ udemy_Geek_universit/exercícios_seção_5/ex030.py
@@ -3,7 +3,7 @@
 lista = []
 
 for _ in range(3):
-    n = int(input('Digite um númro: '))
+    n = int(input('Digite um número: '))
     lista.append(n)
 lista.sort()
 print(f'Assim ficam os números em ordem crescente -> {lista}')
---
+++ udemy_Geek_universit/exercícios_seção_5/ex031.py
@@ -0,0 +1,12 @@
+""" Faça um programa que receba a altura e o peso de uma pessoa. De acordo com a tabela a seguir, verifique e mostre qual a classificação dessa pessoa.
+   Altura                        Peso
+                   Menor 60   Entre 60 e 90   Acima de 90 
+   menor que 1,20    A             D             G
+   de 1,20 a 1,70    B             E             H
+   maior que 1,70    C             F             I
+"""
+alt = float(input('Digite a sua altura: '))
+pes = float(input('Digite o seu peso: '))
+
+
+             
