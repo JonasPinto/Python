ca7289112e68c41ad4d8ec632a56b33bfa92257d
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-31 00:46:09 -0300

    Create ex022.py

---
+++ udemy_Geek_universit/exercícios_seção_4/ex022.py
@@ -0,0 +1,5 @@
+# Leia um valor de comprimento em jardas e apresente-o convertido em metros M = 0.91 * J
+
+j = float(input('Digite o comprimento em jardas: '))
+m = j * 0.9144
+print(f'{j} Jardas correspondem a {m:.2f} metros')
