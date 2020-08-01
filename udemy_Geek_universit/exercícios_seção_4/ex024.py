922be89e1080d869192d68c485770f503383e0a0
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-07-31 23:44:04 -0300

    Create ex024.py

---
+++ udemy_Geek_universit/exercícios_seção_4/ex024.py
@@ -0,0 +1,5 @@
+# Leia um valor de area em metros quadrados m² e epresente-o em acres A = m * 0.000247
+
+m = float(input('Digite a area em m²: '))
+a = m * 0.000247
+print(f'{m} m² correspondem a {a:.2f} acres')
