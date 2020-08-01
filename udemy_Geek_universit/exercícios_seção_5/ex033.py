da001572dbc83eb470ca5117c76d91767c1d1269
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-01 01:27:25 -0300

    Create ex033.py

---
+++ udemy_Geek_universit/exercícios_seção_5/ex033.py
@@ -0,0 +1,8 @@
+"""Um produto vai sofrer almento de acordo com tabela abaixo. Leia o preço antigo, caucule e escreva o preço novo, escreva uma mensagem em função do preço novo (de acordo com a segunda tabela)
+
+Preço antigo           Aumento            Preço novo                Mensagem
+até R$ 50                5%               até R$ 80                 barato
+entre R$ 50 e R$ 100     10%              entre R$ 80 e R$ 120      normal
+acima de R$ 100          15%              entre R$ 120 e R$ 200     caro
+                                          acima de R$ 200           muito caro
+"""
