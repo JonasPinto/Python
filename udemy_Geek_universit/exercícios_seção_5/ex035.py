95f0468c8908ec818340ad63c6cb5a1732f1ae35
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-02 13:46:59 -0300

    Create ex035.py

---
+++ udemy_Geek_universit/exercícios_seção_5/ex035.py
@@ -0,0 +1,3 @@
+"""
+Leia uma data e determine se ela é valida. Ou seja, verifique se o mês está entre 1 e 12, e se o dia existe naquele mês. Note que fevereiro tem 29 dias em anos bissextos, e 28 dias em anos não bissextos 
+"""
