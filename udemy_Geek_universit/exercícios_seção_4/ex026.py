6859a19a3b3bb6a2cc29e369079fbb3eef0cd227
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-02 14:10:35 -0300

    Create ex026.py

---
+++ udemy_Geek_universit/exercícios_seção_4/ex026.py
@@ -0,0 +1,5 @@
+# Leia um valor de area em m² e apresente-o convertido em hectare  h = m * 0.0001
+
+m = float(input('Digite a area em m²: '))
+h = m * 0.0001
+print(f'{m} m² correspondem a {h:.1f} hectares')
