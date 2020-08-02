8fffc9259b57b62e7c4fdbf78228c939a87cd661
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-02 13:48:57 -0300

    Create ex025.py

---
+++ udemy_Geek_universit/exercícios_seção_4/ex025.py
@@ -0,0 +1,6 @@
+# Leia um valor de area em acres e apresente-o convertido em m² m = A * 4048.54
+
+a = float(input('Digite a area em acre: '))
+m = a * 4048.54
+print(f'{a} acres equivalem a {int(m)} m²')
+
