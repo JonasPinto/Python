571ec9735f8a2aba8696f284f9013009dfbfee6a
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-03 23:55:43 -0300

    Create ex031.py

---
+++ udemy_Geek_universit/exercícios_seção_4/ex031.py
@@ -0,0 +1,7 @@
+# Leia um numero inteiro e imprima o seu antecessor e o seu sucessor.
+
+n = int(input('Digite um número: '))
+an = n - 1
+su = n + 1
+print(f'O antecessero de {n} é -> {an}')
+print(f'O sucessor de {n} é -> {su}')
