4c2e8828e6d88f98d864c7d97d8bef15e1f4ae4f
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-04 18:04:29 -0300

    Create ex034.py

---
+++ udemy_Geek_universit/exercícios_seção_4/ex034.py
@@ -0,0 +1,5 @@
+# Leia o valor do raio de um circulo e caucule e imprima a area A = pi * r²
+
+r = float(input('Digite o raio: '))
+a = (r * r) * 3.1416
+print(f'A area do circulo é : \033[35m{a}\033[m')
