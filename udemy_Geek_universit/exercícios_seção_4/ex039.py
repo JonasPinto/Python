827d5c0cc744869b99c1b9d997dd9ff7e2520e11
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-04 21:19:42 -0300

    Create ex039.py

---
+++ udemy_Geek_universit/exercícios_seção_4/ex039.py
@@ -0,0 +1,11 @@
+# A importancia de R$780.000 sera dividida por tres ganhadores de um concurso
+# - O primeiro ira receber 45 %
+# - O segundo recebera 32 %
+# - O terceiro recebera o restante
+
+p = 780_000
+print(f'Com 45% do premio o primeiro ganhador recebera R$ {int(p * 0.45)}')
+print(f'Com 26% do premio o segundo ganhador recebera R$ {int(p * 0.32)}')
+print(f'R$ {int(p * 0.23)} sera do último ganhador que ganhou 23% do premio total')
+
+
