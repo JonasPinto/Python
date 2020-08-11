d761dbca45fbe23a48364a3a42fa0cafa22c2563
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-10 22:06:11 -0300

    Create ex005.py

---
+++ udemy_Geek_universit/exercícios_seção_5/ex005.py
@@ -0,0 +1,7 @@
+# Faça um programa que receba um número inteiro e verifique se este número é par ou impar
+num = int(input('Digite um número inteiro: '))
+if num % 2 == 0:
+    print('O número digitado é par')
+else:
+    print('O número digitado é impar')
+
