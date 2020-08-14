366bea9392bb78692620e4acfab65604b4028b2f
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-13 22:42:38 -0300

    Update ex011.py

--- udemy_Geek_universit/exercícios_seção_5/ex011.py
+++ udemy_Geek_universit/exercícios_seção_5/ex011.py
@@ -3,11 +3,13 @@ Escreva um programa que leia um número inteiro maior do que 0 e devolva, na tel
 Por exemplo, ao número 251 corresponderá o valor 8 (2 + 5 + 1). Se o número lido não for maior do que 0, o programa
 terminará com a mensagem "Número inválido".
 """
+import sys
 n = int(input('Digite um número: '))
 soma = 0
 
 if n <= 0:
     print('Número inválido')
+    sys.exit()
 else:
     while n > 0:
         resto = n % 10
