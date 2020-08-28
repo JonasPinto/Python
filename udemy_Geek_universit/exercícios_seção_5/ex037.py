cbd1c31098437b2c7a757fc841cbcf4a8a565967
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-08-28 02:09:29 -0300

    Create ex037.py

---
+++ udemy_Geek_universit/exercícios_seção_5/ex037.py
@@ -0,0 +1,23 @@
+"""
+As tarifas de certo parque de estacionamento são as seguintes:
+
+1ª e 2ª hora - R$ 1,00 cada
+3ª e 4ª hora - R$ 1,40 cada
+5ª e seguintes - R$ 2,00 cada
+
+O número de horas a pagar é sempre inteiro arredondado por excesso. Deste modo, quem estacionar durante 61 minutos pagará por duas horas, que é o mesmo que pagaria se tivesse permanecido 120 minnutos. Os momentos de chegada ao parque e partida deste são apresentados na forma de pares de inteiros, representando horas e minutos. Por exemplo, o par 12 50 representará dez para uma da tarde. Pretende-se criar um programa que, lidos pelo teclado os momentos de chegada e partida, escreva na tela o preço cobrado pelo estacionamento. Admite-se que a chegada e a partida se dão com intervalo não superior a 24 horas. Portanto, se uma dada hora de chegada for superior á da partida, isso não é uma cituação de erro, antes significará que a partida ocorreu no dia seguinte ao da chegada.   
+"""
+print('-----------------------------------------')
+print('             PARQUIMETRO                 ')
+print('-----------------------------------------')
+print('Formato de hora 24h 00:00')
+
+chegada = input('Digite o horaio de entrada -> ')
+saida = input('Digite o horario de saída    -> ')
+
+hora_chegada = int(chegada[:2])
+minutos_chegada = int(chegada[3:])
+
+
+print(hora_chegada)
+print(minutos_chegada)
