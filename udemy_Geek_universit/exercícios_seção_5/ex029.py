998665654982919cb10167a53157b91a988e44c1
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-09-12 21:09:43 -0300

    Update ex029.py

--- udemy_Geek_universit/exercícios_seção_5/ex029.py
+++ udemy_Geek_universit/exercícios_seção_5/ex029.py
@@ -2,31 +2,31 @@
 
 from random import randint
 
-list_in = []
-list_out_a = []
-list_out_b = []
-list_r = []
-i = 1
-qok = 0
-qno = 0
-for i in range (0, 5):
+list_in = [] # lista de entrada
+list_out_a = [] # lista de saida a
+list_out_b = [] # lista de saida b
+list_r = [] # lista de respostas
+i = 1 # variavel de controle para o laço for
+r_ok = 0 # quantidade de respostas corretas
+r_err = 0 # quntidade de respostas incorretas
 
-    a = randint(1, 10)
+for i in range (0, 5): 
+    a = randint(1, 10) # atribui números aleatórios as variaveis a e b
     b = randint(1, 10)       
-    print(f'Pergunta: Qual é a soma de {a} + {b} ?')
-    list_in.append(input('   Resposta: '))
-    list_out_a.append(a)
+    print(f'Pergunta: Qual é a soma de {a} + {b} ?') # mostra a pergunta com números aleatórios 
+    list_in.append(input('Resposta: ')) # guarda as respostas de entrada do usuario
+    list_out_a.append(a) # o número aleatorio gerado é guardado na lista de saída a e b
     list_out_b.append(b)
-    list_r.append(a + b)
+    list_r.append(a + b) # executa a soma entre a e b a guarda o resultado na lista de resposta
 
 for i in range(0,5):
-    print(f'A soma de {list_out_a[i]} + {list_out_b[i]} = {list_r[i]}')
+    print(f'\nA soma de {list_out_a[i]} + {list_out_b[i]} = {list_r[i]}')
     if list_in[i] == str(list_r[i]):
-        print(f'Sua resposta = {list_in[i]}')
-        print(f'\033[94mResposta correta\033[m\n')
-        qok += 1
+        print(f'Sua resposta = {list_in[i]}') # faz comparação entre a soma gerada e o resultado, se verdadeiro (r_ok -> resposta correta) recebe + 1  se nao (r_err -> resposta errada recebe + 1)
+        print(f'Resposta correta\n')
+        r_ok += 1
     else:
         print(f'Sua resposta = {list_in[i]}')
-        print(f'\033[31mResposta incorreta\033[m\n')
-        qno += 1
-print(f'\033[94mAcertos\033[m = {qok} questões\n\033[31mErros\033[m = {qno} questões')
+        print(f'Resposta incorreta\n')
+        r_err += 1
+print(f'Acertos = {r_ok} questões\nErros = {r_err} questões') # mostra na tela quantas respostas foram corretas e quantas foram incorretas
