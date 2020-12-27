0536a36b9e3cfda86596b1aa17b78692e47f2a0e
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2020-12-27 18:25:20 -0300

    Update ex035.py

--- udemy_Geek_universit/exercícios_seção_5/ex035.py
+++ udemy_Geek_universit/exercícios_seção_5/ex035.py
@@ -4,7 +4,7 @@ Leia uma data e determine se ela é valida. Ou seja, verifique se o mês está e
 import sys
 mes_str = {1: '31Janeiro', 2: '', 3: '31Março', 4: '30Abril', 5: '31Maio', 6: '30Junho', 7: '31Julho', 8: '31Agosto', 9: '30Setembro', 10: '31Outubro', 11: '30Novembro', 12: '31Dezembro'}
 
-data = str(input('digite a data no formato DD/MM/YY\n'))
+data = str(input('digite a data no formato DD/MM/YYYY\n'))
 if len(data) == 10:
     dia = data[:2]
     mes = int(data[3:5])
