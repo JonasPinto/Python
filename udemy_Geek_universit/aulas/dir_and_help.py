9324fbcc9dc16eff63e2524ea1594a6a57e1d2c0
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2021-01-04 23:32:04 -0300

    Update dir_and_help.py

--- udemy_Geek_universit/aulas/dir_and_help.py
+++ udemy_Geek_universit/aulas/dir_and_help.py
@@ -4,7 +4,7 @@ utilitarios python para auxiliar na programção.
 
 dir -> apresenta todos os atributos/propriedades e funções/metodos disponiveis
 para determinado tipo de dado ou variavel.
-    ex: dir(math), dir(int), dir(tipo de dado ou biblioteca), etc...
+    ex: dir('math'), dir('int'), dir('tipo de dado ou biblioteca'), etc...
 
 help -> apresenta a documentação/como utilizar  os atributos/proriedades e funções/metodos disponíveis
 para determinado tipo de dado ou variável.
