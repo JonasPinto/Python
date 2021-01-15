f109f8b44458569add02b2d68bd48f88ce5ddc23
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2021-01-14 23:16:08 -0300

    Update ex002.py

--- udemy_Geek_universit/exercicios_seção_6/ex002.py
+++ udemy_Geek_universit/exercicios_seção_6/ex002.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que escreva na tela de 1 até 100, de 1 em 1, 3 vezes. Aprimeira vez usando a estrutura de repetição for, a segunda utilizando while.
+'''
+
 for n in range(1, 101):
     print(n,end=' ')
 
@@ -6,5 +10,5 @@ print('\n')
 n = 0
 while n < 100:
     n = n + 1
-    print(n, end=' ')
+    print(n,end=' ')
 
