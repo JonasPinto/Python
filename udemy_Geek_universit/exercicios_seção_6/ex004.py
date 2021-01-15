bbd6a1f2382280928ba63918412a63479ca1d4ec
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2021-01-14 23:16:20 -0300

    Create ex004.py

---
+++ udemy_Geek_universit/exercicios_seção_6/ex004.py
@@ -0,0 +1,4 @@
+'''
+Escreva um programa que declare um número inteiro, inicialize-o com 0, e incremente-o de 10000 em 1000, imprimindo seu valor na tela, até que seu valor seja 1000000(cem mil). 
+
+'''
