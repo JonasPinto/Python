7a8856a953399769a2ca765f93194c968fe94b18
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2021-01-31 17:47:43 -0300

    Update ex003.py

--- udemy_Geek_universit/exercícios_seção_4/ex003.py
+++ udemy_Geek_universit/exercícios_seção_4/ex003.py
@@ -4,4 +4,4 @@ n1 = int(input('Digite o 1º valor: '))
 n2 = int(input('Digite o 2º valor: '))
 n3 = int(input('Digite o 3° valor: '))
 s = n1 + n2 + n3
-print(f'A soma dos valores digitados é de \033[32m {s}')
+print(f'A soma dos valores digitados é de \033[32m {s}\033[m')
