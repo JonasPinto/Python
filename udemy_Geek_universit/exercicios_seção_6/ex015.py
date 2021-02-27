80a7012f5af8b390969d837f0e80ab6a34934edc
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2021-02-27 17:22:25 -0300

    Create ex015.py

---
+++ udemy_Geek_universit/exercicios_seção_6/ex015.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa que um número inteiro positivo impar N e imprima todos os números impares de 1 até N em ordem crescente
+'''
+from random import random
+lista = []
+
+while True:
+    var = random()
+    lista.append(round(var))
+    if len(lista) == 10000:
+        break
+print(var)
+print(lista)
+print(type(var))
