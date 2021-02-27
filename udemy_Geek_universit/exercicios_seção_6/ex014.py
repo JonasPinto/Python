6e5cf33101bedd2260a6585c4ff042a3a3cbd7c2
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2021-02-27 17:22:19 -0300

    Update ex014.py

--- udemy_Geek_universit/exercicios_seção_6/ex014.py
+++ udemy_Geek_universit/exercicios_seção_6/ex014.py
@@ -2,12 +2,16 @@
 Faça um programa que leia um número inteiro positivo par N, e imprima todos os números pares de 0 até N em ordem decrescente
 '''
 import time
+resp = ''
 while True:
     n = int(input('Digite um número inteiro, positivo e par: '))
     if n > 0 and n % 2 == 0:
-        break
+        for i in range(n, 0, -2):
+            time.sleep(0.2)
+            print(i, end= ' ')
     else:
-        time.sleep(0.4)
         print('Número inválido!')
-for i in range(n, 0, -2):
-    print(i, end= ' ')
+    resp = input('\nENTER -> Continuar: \nS -> Sair: ')
+    if resp == 's':
+        break
+    
