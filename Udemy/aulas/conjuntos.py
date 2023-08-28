e7ea1a6ced36c19fdb6037b51fcd49998e2ccb8b
Jonas Silva <jonas.silva@easolucinfo.com>
2023-08-28 20:26:04 -0300

    Update conjuntos.py

--- Udemy/aulas/conjuntos.py
+++ Udemy/aulas/conjuntos.py
@@ -169,3 +169,11 @@ print(len(s))
 
 # ['__and__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__iand__', '__init__', '__init_subclass__', '__ior__', '__isub__', '__iter__', '__ixor__', '__le__', '__len__', '__lt__', '__ne__', '__new__', '__or__', '__rand__', '__reduce__', '__reduce_ex__', '__repr__', '__ror__', '__rsub__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__xor__', 'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update']
 """
+# soma*, Valor Maximo*, Valor Minimo*, Tamanho
+# Se os valores forem todos inteiros ou reais
+s = {1, 2, 3, 4, 5, 6}
+
+print(sum(s))
+print(max(s))
+print(min(s))
+print(len(s))
